b182d85a679a99f0fb44f2393122174e258b5d36
Jacob Robertson <g.jacob.robertson@gmail.com>
2011-09-28 12:34:14 -0400

    Rename Sample Field to Location

--- webapp/core/models.py
+++ webapp/core/models.py
@@ -6,12 +6,10 @@ class Loan(models.Model):
     '''
     Represents an item loaned to an individual.
     '''
-    # An example of how to define fields, will be removed once real fields are
-    # added
-    sample_field = models.CharField(max_length=100, verbose_name="Foo")
+    location_field = models.CharField(max_length=100, verbose_name="Location")
 
     def __unicode__(self):
-        return self.sample_field
+        return self.id
 
 
 class LoanForm(ModelForm):
