8b94a8033cfd91be95522f2b9f9862149232cc17
Ben Olive <ben.olive@hireiqinc.com>
2011-09-30 17:25:33 -0400

    Prompt to update on change

--- webapp/core/models.py
+++ webapp/core/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     '''
     An item that TSO can loan out.
     '''
+    serial_number = models.CharField(blank=True, null=True, max_length=255, unique=True)
     description = models.TextField(blank=False)
 
 
@@ -19,12 +20,26 @@ class Loan(models.Model):
         return self.sample_field
 
 
-class LoanForm(ModelForm):
+class DivFormMixin:
+    '''
+    Mix this into a django.forms.Form to enable a new output method 'as_div' that
+    will output divs for use with blueprint.css
+    '''
+    def as_div(self):
+        "Returns this form rendered as HTML <divs>s"
+        return self._html_output(
+            normal_row=u'<div class="span-13 last"><div class="span-3">%(label)s%(errors)s</div><div class="span-10 last">%(field)s</div>%(help_text)s</div>',
+            error_row=u'<div class="error span-13 last">%s</div>',
+            row_ender=u'</div>',
+            help_text_html = u'<div class="quiet legend prepend-3 span-10 last">%s</div>',
+            errors_on_separate_row=False)
+
+class LoanForm(ModelForm, DivFormMixin):
     class Meta:
         model = Loan
         exclude = ('item')
 
 
-class ItemForm(ModelForm):
+class ItemForm(ModelForm, DivFormMixin):
     class Meta:
         model = Item
